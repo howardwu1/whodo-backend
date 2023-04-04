1fe971b931a6eb26429be0938da9d643c44d5b0f
howard23 <howard@radiusxr.com>
2023-04-03 21:56:52 -0500

    added initialzr stuff

---
+++ src/main/java/com/whodo/whodobackend/WhodoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.whodo.whodobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhodoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhodoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/whodo/whodobackend/WhodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.whodo.whodobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
